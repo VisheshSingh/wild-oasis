fbe97d020d0bd7b6ed6e67470d96ffc01f6cb429
Vishesh Singh <VisheshSingh16@gmail.com>
2024-08-03 13:20:15 -0400

    setting stateTime for react query

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ import AppLayout from './ui/AppLayout';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 60 * 1000,
-      staleTime: 0,
+      staleTime: 60 * 1000,
     },
   },
 });
