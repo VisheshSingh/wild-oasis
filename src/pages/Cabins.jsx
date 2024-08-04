0ce4d9b0a390d7523ad83c1820c0fac3726b6717
Vishesh Singh <VisheshSingh16@gmail.com>
2024-08-03 20:17:01 -0400

    React Hook Form 📜

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
+import Button from '../ui/Button';
 import CabinTable from '../features/cabins/CabinTable';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type='horizontal'>
@@ -10,8 +14,10 @@ function Cabins() {
         <p>Filter/Sort</p>
       </Row>
 
-      <Row type='horizontal'>
+      <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm(!showForm)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
